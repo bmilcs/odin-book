ed0cdcc95f69837f34dc74edd0482962c4caeaa7
Bryan Miller <bmilcs@yahoo.com>
2023-10-29 15:27:55 -0400

    Add server tryCatch wrapper to utils

---
+++ server/src/utils/tryCatch.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+type Controller = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+// This is a utility function that wraps a controller function
+// and handles any errors that it throws.
+
+const tryCatch =
+  (controller: Controller) =>
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // Call the controller function and pass it the request, response, and
+      // next function.
+      await controller(req, res, next);
+    } catch (error) {
+      // Call the next function and pass it the error.
+      next(error);
+    }
+  };
+
+export default tryCatch;
