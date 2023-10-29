ac2c6deea2c7f4de8317693065541c44609bc573
Bryan Miller <bmilcs@yahoo.com>
2023-10-29 15:52:16 -0400

    Add errorResponder middleware to server

---
+++ server/src/middleware/errorResponder.ts
@@ -0,0 +1,23 @@
+import AppError from '@/utils/AppError';
+import ValidationError from '@/utils/ValidationError';
+import { NextFunction, Request, Response } from 'express';
+
+const errorResponder = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  // handle validation errors
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json({ type: err.name, message: err.message, errors: err.errorArray });
+  }
+
+  // handle app errors
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      type: err.name,
+      message: err.message,
+    });
+  }
+
+  // handle other errors
+  res.status(500).json({ type: 'ServerError', message: 'internal server error' });
+};
+
+export default errorResponder;
