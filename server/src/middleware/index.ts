0edb94f1805a1b1ea38671b5391fa57a77930e8e
Bryan Miller <bmilcs@yahoo.com>
2023-10-29 15:52:56 -0400

    Add index to server's middleware/ directory

---
+++ server/src/middleware/index.ts
@@ -0,0 +1,7 @@
+export { default as errorLogger } from '@/middleware/errorLogger';
+
+export { default as errorResponder } from '@/middleware/errorResponder';
+
+export { default as handleValidationErrors } from '@/middleware/handleValidationErrors';
+
+export { default as invalidPathHandler } from '@/middleware/invalidPathHandler';
