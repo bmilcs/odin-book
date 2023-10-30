0a8af09286e40d490bd5228ad4adaa1cedc89f3a
Bryan Miller <bmilcs@yahoo.com>
2023-10-30 16:14:33 -0400

    Add sever likeModel

---
+++ server/src/models/likeModel.ts
@@ -0,0 +1,21 @@
+import { Document, Schema, model } from 'mongoose';
+import { IComment } from './commentModel';
+import { IPost } from './postModel';
+import { IUser } from './userModel';
+
+export interface ILike extends Document {
+  user: IUser['_id'];
+  post?: IPost['_id'];
+  comment?: IComment['_id'];
+}
+
+const likeSchema: Schema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: Schema.Types.ObjectId, ref: 'Post' },
+  comment: { type: Schema.Types.ObjectId, ref: 'Comment' },
+});
+
+// A user can only like a post or comment once
+likeSchema.index({ post: 1, comment: 1 }, { unique: true, sparse: true });
+
+export default model<ILike>('Like', likeSchema);
