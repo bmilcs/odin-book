7a773a44497214656242112ec6af876b95440b08
Bryan Miller <bmilcs@yahoo.com>
2023-10-30 16:14:23 -0400

    Add sever commentModel

---
+++ server/src/models/commentModel.ts
@@ -0,0 +1,17 @@
+import { IPost } from '@/models/postModel';
+import { IUser } from '@/models/userModel';
+import { Document, Schema, model } from 'mongoose';
+
+export interface IComment extends Document {
+  text: string;
+  author: IUser['_id'];
+  post: IPost['_id'];
+}
+
+const CommentSchema: Schema = new Schema({
+  text: { type: String, required: true, maxlength: 1000 },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+});
+
+export default model<IComment>('Comment', CommentSchema);
