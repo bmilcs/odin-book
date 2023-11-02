7a04c576f7bd812653a307b00e1594cea258f54e
Bryan Miller <bmilcs@yahoo.com>
2023-11-02 17:51:18 -0400

    Add server logic to signup controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,17 +1,36 @@
-import { tryCatch } from '@/utils';
+import { userModel } from '@/models';
+import { AppError, tryCatch } from '@/utils';
 import { NextFunction, Request, Response } from 'express';
 
-const signup = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('signup');
-});
+const signup = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // validation & duplicate checks occur in previous middleware
+    const { email, password, username } = req.body;
+    const user = await userModel.create({
+      email,
+      password,
+      username,
+    });
+    if (!user) {
+      throw new AppError('Unable to create a user at this time', 500);
+    }
+    const data = { userId: user.id };
+    res.addJwtCookies(user.id);
+    res.success('User created successfully', data, 200);
+  },
+);
 
-const login = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('login');
-});
+const login = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json('login');
+  },
+);
 
-const logout = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('logout');
-});
+const logout = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json('logout');
+  },
+);
 
 export default {
   signup,
