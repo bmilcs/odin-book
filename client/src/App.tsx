060fa30578a1e32916f5af55cd985f0988259278
Bryan Miller <bmilcs@yahoo.com>
2023-11-05 16:47:03 -0500

    Add theme-provider & routes to App component on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import Layout from '@/components/layout';
+import Home from '@/components/pages/home';
+import Login from '@/components/pages/login';
+import Signup from '@/components/pages/signup';
+import { ScrollToTop } from '@/components/services/scroll-to-top';
+import { ThemeProvider } from '@/components/services/theme-provider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <h1>FriendLink</h1>;
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Layout>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/login" Component={Login} />
+            <Route path="/signup" Component={Signup} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
