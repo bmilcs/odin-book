dffe88f66f1ea745aecbd16ccc7ceed2df77e299
Bryan Miller <bmilcs@yahoo.com>
2023-11-05 16:48:16 -0500

    Change footer link to GitHub repo

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="">
       <div className="mx-auto flex max-w-7xl justify-center border-t-2 p-2">
         <a
-          href="https://bmilcs.com"
+          href="https://www.github.com/bmilcs/odin-book"
           className="flex gap-2  transition-colors duration-300 ease-in-out hover:text-blue-500"
         >
           Created by BMILCS
