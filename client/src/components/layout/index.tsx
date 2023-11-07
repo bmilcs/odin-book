f3e552dc277a4f0afbf5ac51c519f201ba0c5221
Bryan Miller <bmilcs@yahoo.com>
2023-11-07 16:42:14 -0500

    Fix layout index styling on client

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -5,7 +5,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <main className="mx-auto max-w-7xl flex-1 p-2 md:p-4">{children}</main>
+      <main className="flex-1">
+        <div className="mx-auto min-h-full max-w-7xl p-4 md:p-10 lg:p-14">
+          {children}
+        </div>
+      </main>
       <Footer />
     </>
   );
