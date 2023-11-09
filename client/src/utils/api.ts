728097f3e06c799435387d759890b28898687f0d
Bryan Miller <bmilcs@yahoo.com>
2023-11-09 16:27:35 -0500

    Add client utils/api: fetch wrapper for api calls

---
+++ client/src/utils/api.ts
@@ -0,0 +1,78 @@
+import { API_BASE_URL } from '@/utils/env';
+
+/**
+ * HTTP methods supported by the API.
+ */
+type Method = 'GET' | 'POST' | 'DELETE' | 'PATCH';
+
+/**
+ * Calls the API with the specified HTTP method, path and data.
+ * @param method The HTTP method to use.
+ * @param path The path to the API endpoint.
+ * @param data Optional data to send with the request.
+ * @returns A Promise that resolves to the response data.
+ * @throws An error if the API request fails.
+ */
+async function callApi<T>(
+  method: Method,
+  path: string,
+  data?: Record<string, unknown>,
+): Promise<T> {
+  const url = `${API_BASE_URL}${path}`;
+  const options: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: data ? JSON.stringify(data) : undefined,
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const responseData = await response.json();
+    return responseData as T;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Something went wrong with the API request');
+  }
+}
+
+/**
+ * Object containing functions to make API requests using different HTTP methods.
+ */
+const api = {
+  /**
+   * Sends a GET request to the specified API endpoint.
+   * @param path The path to the API endpoint.
+   * @returns A Promise that resolves to the response data.
+   */
+  get: <T>(path: string): Promise<T> => callApi<T>('GET', path),
+
+  /**
+   * Sends a POST request to the specified API endpoint with the specified data.
+   * @param path The path to the API endpoint.
+   * @param data The data to send with the request.
+   * @returns A Promise that resolves to the response data.
+   */
+  post: <T>(path: string, data: Record<string, unknown>): Promise<T> =>
+    callApi<T>('POST', path, data),
+
+  /**
+   * Sends a DELETE request to the specified API endpoint.
+   * @param path The path to the API endpoint.
+   * @returns A Promise that resolves to the response data.
+   */
+  del: <T>(path: string): Promise<T> => callApi<T>('DELETE', path),
+
+  /**
+   * Sends a PATCH request to the specified API endpoint with the specified data.
+   * @param path The path to the API endpoint.
+   * @param data The data to send with the request.
+   * @returns A Promise that resolves to the response data.
+   */
+  patch: <T>(path: string, data: Record<string, unknown>): Promise<T> =>
+    callApi<T>('PATCH', path, data),
+};
+
+export default api;
