4ce7acbc3cf023df1353d3e74d0423733f37efca
Bryan Miller <bmilcs@yahoo.com>
2023-11-11 18:08:11 -0500

    Add AuthProvider to App on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,27 @@ import Layout from '@/components/layout';
 import Home from '@/components/pages/home';
 import Login from '@/components/pages/login';
 import Signup from '@/components/pages/signup';
+import AuthProvider from '@/components/services/auth-provider';
 import { ScrollToTop } from '@/components/services/scroll-to-top';
 import { ThemeProvider } from '@/components/services/theme-provider';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Layout>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/login" Component={Login} />
-            <Route path="/signup" Component={Signup} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Layout>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="/login" Component={Login} />
+              <Route path="/signup" Component={Signup} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
