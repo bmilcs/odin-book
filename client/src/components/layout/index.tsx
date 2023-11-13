69a009e21f2f313146d6ddd12ea8abbcae404e2c
Bryan Miller <bmilcs@yahoo.com>
2023-11-13 16:21:52 -0500

    Fix page layout index styling

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -5,8 +5,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <main className="flex-1">
-        <div className="mx-auto min-h-full max-w-7xl p-4 md:p-10 lg:p-14">
+      <main className="flex flex-grow">
+        <div className="mx-auto min-h-full max-w-7xl flex-grow p-4 md:p-10 lg:p-14">
           {children}
         </div>
       </main>
