d240bec31afba7e995f24360bf22d64668d8ceca
Bryan Miller <bmilcs@yahoo.com>
2023-11-13 16:22:22 -0500

    Add header dropdown for logged in users

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,22 +1,71 @@
+import { AuthContext } from '@/components/services/auth-provider';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { ModeToggle } from '@/components/ui/mode-toggle';
+import { GearIcon } from '@radix-ui/react-icons';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { isAuthenticated, logout } = useContext(AuthContext);
+
   return (
     <header>
       <div className="mx-auto flex max-w-7xl items-center justify-between border-b-2 p-2">
         <p className="text-lg font-black">FriendLink</p>
         <nav>
           <ul className="flex items-center font-normal">
-            <li className="px-2">
-              <Link to={'/'}>Home</Link>
-            </li>
-            <li className="px-2">
-              <Link to={'/login'}>Login</Link>
-            </li>
-            <li className="px-2">
-              <Link to={'/signup'}>Signup</Link>
-            </li>
+            {isAuthenticated() ? (
+              //
+              // User is Logged In
+              //
+              <>
+                <li className="px-2">
+                  <Link to={'/feed'}>Feed</Link>
+                </li>
+                <li className="px-2">
+                  <Link to={'/profile'}>Profile</Link>
+                </li>
+                <li>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline" size="icon">
+                        <GearIcon />
+                        <span className="sr-only">Settings Menu</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem onClick={logout}>
+                        Settings
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={logout}>
+                        Logout
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </li>
+              </>
+            ) : (
+              //
+              // User is Logged Out
+              //
+              <>
+                <li className="px-2">
+                  <Link to={'/login'}>Login</Link>
+                </li>
+                <li className="px-2">
+                  <Link to={'/signup'}>Signup</Link>
+                </li>
+              </>
+            )}
+            {/* 
+            Theme Toggle 
+            */}
             <li className="px-2">
               <ModeToggle />
             </li>
