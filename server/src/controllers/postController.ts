668e04f59494a7ffe96c52cdb41131399bf51a13
Bryan Miller <bmilcs@yahoo.com>
2023-11-16 17:07:52 -0500

    Add functionality to postController's createPost

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,41 +1,70 @@
-import { tryCatch } from '@/utils';
+import { postModel } from '@/models';
+import { AppError, tryCatch } from '@/utils';
 import { NextFunction, Request, Response } from 'express';
 
-const createPost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('create post');
-});
+const createPost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { content } = req.body;
+    if (!content) {
+      return next(new AppError('Please provide content for your post', 400));
+    }
+    const post = await postModel.create({ author: req.userId, content });
+    if (!post) {
+      return next(
+        new AppError('Unable to create a post at this time', 500, 'AppError'),
+      );
+    }
+    res.success('Post created', post, 201);
+  },
+);
 
-const getPost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('view post');
-});
+const getPost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('get post');
+  },
+);
 
-const updatePost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('update post');
-});
+const updatePost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('update post');
+  },
+);
 
-const deletePost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('delete post');
-});
+const deletePost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('delete post');
+  },
+);
 
-const likePost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('like post');
-});
+const likePost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('like post');
+  },
+);
 
-const unlikePost = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('unlike post');
-});
+const unlikePost = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('unlike post');
+  },
+);
 
-const addComment = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('add post comment');
-});
+const addComment = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('add post comment');
+  },
+);
 
-const editComment = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('edit post comment');
-});
+const editComment = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('edit post comment');
+  },
+);
 
-const deleteComment = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('delete post comment');
-});
+const deleteComment = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.success('delete post comment');
+  },
+);
 
 export default {
   createPost,
