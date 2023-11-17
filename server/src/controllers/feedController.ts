10a6d879d4a34bd4ffd9ad002a22b820d36f388f
Bryan Miller <bmilcs@yahoo.com>
2023-11-17 16:50:12 -0500

    Add feedController functionality to getFeed on server

--- server/src/controllers/feedController.ts
+++ server/src/controllers/feedController.ts
@@ -1,9 +1,30 @@
-import { tryCatch } from '@/utils';
+import { postModel, userModel } from '@/models';
+import { AppError, tryCatch } from '@/utils';
 import { NextFunction, Request, Response } from 'express';
 
-const getFeed = tryCatch(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('get feed');
-});
+const getFeed = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req;
+    const user = await userModel.findById(userId, { friends: 1 });
+    if (!user) {
+      return next(new AppError('User not found', 400));
+    }
+    const posts = await postModel
+      .find({
+        $or: [{ author: { $in: user.friends } }, { author: user._id }],
+      })
+      .populate('author', {
+        password: 0,
+        friendRequestsSent: 0,
+        friendRequestsReceived: 0,
+      })
+      .populate('likes')
+      .populate('comments')
+      .sort({ date: -1 });
+
+    res.success('Feed retrieved', posts, 200);
+  },
+);
 
 export default {
   getFeed,
