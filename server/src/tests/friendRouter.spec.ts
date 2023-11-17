2229599c10f9325b49105f2568d4299546f06fe6
Bryan Miller <bmilcs@yahoo.com>
2023-11-17 12:51:20 -0500

    Add server friendRouter tests: reject friend request / delete friend

--- server/src/tests/friendRouter.spec.ts
+++ server/src/tests/friendRouter.spec.ts
@@ -108,7 +108,7 @@ describe('FRIEND ROUTER', () => {
       expect(body.error).to.equal('Unauthorized');
     });
 
-    it('should return 400 if friend request id is invalid', async () => {
+    it('should return 400 if friend id is invalid', async () => {
       const { statusCode, body } = await request(app)
         .patch('/friends/accept-request/1')
         .set('Cookie', USER_ONE.jwtCookie)
@@ -118,7 +118,7 @@ describe('FRIEND ROUTER', () => {
       expect(body.error).to.equal('Friend user ID is invalid');
     });
 
-    it('should return 400 if friend request id is not found', async () => {
+    it('should return 400 if friend id is not found', async () => {
       const nonexistentMongoID = '111111111111111111111111';
       const { statusCode, body } = await request(app)
         .patch(`/friends/accept-request/${nonexistentMongoID}`)
@@ -164,6 +164,10 @@ describe('FRIEND ROUTER', () => {
     });
   });
 
+  //
+  // REJECT FRIEND REQUEST
+  //
+
   describe('PATCH /reject-request/:userId', () => {
     it('should return 401 if user is not logged in', async () => {
       const { statusCode, body } = await request(app)
@@ -173,7 +177,62 @@ describe('FRIEND ROUTER', () => {
       expect(body.success).to.be.false;
       expect(body.error).to.equal('Unauthorized');
     });
+
+    it('should return 400 if friend id is invalid', async () => {
+      const { statusCode, body } = await request(app)
+        .patch('/friends/reject-request/1')
+        .set('Cookie', USER_ONE.jwtCookie)
+        .send({});
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Friend user ID is invalid');
+    });
+
+    it('should return 400 if friend id is not found', async () => {
+      const nonexistentMongoID = '111111111111111111111111';
+      const { statusCode, body } = await request(app)
+        .patch(`/friends/reject-request/${nonexistentMongoID}`)
+        .set('Cookie', USER_ONE.jwtCookie)
+        .send({});
+
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('User not found');
+    });
+
+    it('should return 400 if friend request is not found', async () => {
+      const { statusCode, body } = await request(app)
+        .patch(`/friends/reject-request/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie)
+        .send({});
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Friend request not found');
+    });
+
+    it('should reject a friend request w/ database checks', async () => {
+      // send a friend request from USER_ONE to USER_TWO
+      await request(app)
+        .post(`/friends/send-request/${USER_TWO._id}`)
+        .set('Cookie', USER_ONE.jwtCookie);
+
+      const { statusCode, body } = await request(app)
+        .patch(`/friends/reject-request/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+      expect(statusCode).to.equal(200);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal('Friend request rejected');
+      // check that both user models received the friend request
+      const userOne = await userModel.findById(USER_ONE._id);
+      const userTwo = await userModel.findById(USER_TWO._id);
+      expect(userOne?.friendRequestsSent).to.have.lengthOf(0);
+      expect(userTwo?.friendRequestsReceived).to.have.lengthOf(0);
+    });
   });
+
+  //
+  // DELETE FRIEND
+  //
 
   describe('DELETE /:userId', () => {
     it('should return 401 if user is not logged in', async () => {
@@ -181,6 +240,59 @@ describe('FRIEND ROUTER', () => {
       expect(statusCode).to.equal(401);
       expect(body.success).to.be.false;
       expect(body.error).to.equal('Unauthorized');
+    });
+
+    it('should return 400 if friend id is invalid', async () => {
+      const { statusCode, body } = await request(app)
+        .delete('/friends/1')
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Friend user ID is invalid');
+    });
+
+    it('should return 400 if friend id is not found', async () => {
+      const nonexistentMongoID = '111111111111111111111111';
+      const { statusCode, body } = await request(app)
+        .delete(`/friends/${nonexistentMongoID}`)
+        .set('Cookie', USER_ONE.jwtCookie);
+
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('User not found');
+    });
+
+    it('should return 400 if friend is not found', async () => {
+      const { statusCode, body } = await request(app)
+        .delete(`/friends/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Friend not found');
+    });
+
+    it('should delete a friend w/ database checks', async () => {
+      // send a friend request from USER_ONE to USER_TWO
+      await request(app)
+        .post(`/friends/send-request/${USER_TWO._id}`)
+        .set('Cookie', USER_ONE.jwtCookie);
+      // accept the friend request
+      await request(app)
+        .patch(`/friends/accept-request/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+
+      const { statusCode, body } = await request(app)
+        .delete(`/friends/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+      expect(statusCode).to.equal(200);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal('Friend deleted');
+      // check that both user models received the friend request
+      const userOne = await userModel.findById(USER_ONE._id);
+      const userTwo = await userModel.findById(USER_TWO._id);
+      expect(userOne?.friends).to.have.lengthOf(0);
+      expect(userTwo?.friends).to.have.lengthOf(0);
     });
   });
 });
