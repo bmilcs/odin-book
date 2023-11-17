3f9ad8d1e8632ff38e5faf39070227ab2bd5ae46
Bryan Miller <bmilcs@yahoo.com>
2023-11-17 16:50:37 -0500

    Fix inconstent describe block for invalidPath spec

--- server/src/tests/invalidPath.spec.ts
+++ server/src/tests/invalidPath.spec.ts
@@ -2,7 +2,7 @@ import { app } from '@/tests/setup';
 import { expect } from 'chai';
 import request from 'supertest';
 
-describe('Invalid Paths', () => {
+describe('INVALID PATH', () => {
   it('should respond with 404', async () => {
     const res = await request(app).get('/invalid-path').expect(404);
     expect(res.body.success).to.be.false;
