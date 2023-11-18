85db3149e8131dbbbb460f278c11bc75ba5813b2
Bryan Miller <bmilcs@yahoo.com>
2023-11-18 17:18:10 -0500

    Add utils/formatters to client: formatDate

---
+++ client/src/utils/formatters.ts
@@ -0,0 +1,27 @@
+export function formatDate(date: string): string {
+  const createdAt = new Date(date);
+  const now = new Date();
+  const diff = now.getTime() - createdAt.getTime();
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+
+  if (seconds < 60) {
+    return `moments ago`;
+  } else if (minutes === 1) {
+    return `${minutes} minute ago`;
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 24) {
+    return `${hours} hours ago`;
+  } else if (days < 30) {
+    return `${days} days ago`;
+  } else if (months < 12) {
+    return `${months} months ago`;
+  } else {
+    return `${years} years ago`;
+  }
+}
