89c28450b43ac9d7869a014a72841bcda4ed1aa6
Bryan Miller <bmilcs@yahoo.com>
2023-11-18 17:19:07 -0500

    Update App.tsx on client: FeedProvider & route component name fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import Layout from '@/components/layout';
-import Home from '@/components/pages/home';
-import Login from '@/components/pages/login';
-import Signup from '@/components/pages/signup';
+import FeedPage from '@/components/pages/feed-page';
+import HomePage from '@/components/pages/home-page';
+import LoginPage from '@/components/pages/login-page';
+import SignupPage from '@/components/pages/signup-page';
 import AuthProvider from '@/components/services/auth-provider';
+import FeedProvider from '@/components/services/feed-provider';
 import { ScrollToTop } from '@/components/services/scroll-to-top';
 import { ThemeProvider } from '@/components/services/theme-provider';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -11,16 +13,20 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider>
-          <Layout>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/" Component={Home} />
-              <Route path="/login" Component={Login} />
-              <Route path="/signup" Component={Signup} />
-            </Routes>
-          </Layout>
-        </ThemeProvider>
+        <FeedProvider>
+          <ThemeProvider>
+            <Layout>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/" Component={HomePage} />
+                <Route path="/login" Component={LoginPage} />
+                <Route path="/signup" Component={SignupPage} />
+                <Route path="/feed" Component={FeedPage} />
+                <Route path="*" Component={FeedPage} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </FeedProvider>
       </AuthProvider>
     </BrowserRouter>
   );
