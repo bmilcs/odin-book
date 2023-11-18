ff55b8df2af5eb91982c3e5f4ca8eb902ff71235
Bryan Miller <bmilcs@yahoo.com>
2023-11-18 17:16:40 -0500

    Add FeedProvider & FeedContext to client: fetching feeds

---
+++ client/src/components/services/feed-provider.tsx
@@ -0,0 +1,102 @@
+import { TUser } from '@/components/services/auth-provider';
+import api from '@/utils/api';
+import STATUS from '@/utils/constants';
+import {
+  FC,
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+
+type ApiResponse = {
+  success: boolean;
+  data: TPost[];
+  error: string;
+};
+
+export type TComment = {
+  _id: string;
+  content: string;
+  user: TUser;
+  post: TPost;
+  createdAt: string;
+};
+
+export type TPost = {
+  _id: string;
+  content: string;
+  author: TUser;
+  comments: TComment[];
+  likes: TLike[];
+  createdAt: string;
+};
+
+export type TLike = {
+  _id: string;
+  user: TUser;
+  post?: TPost;
+  comment?: TComment;
+  createdAt: string;
+};
+
+type FeedContextProps = {
+  status: string;
+  error: string;
+  feed: TPost[];
+  updateFeed: () => Promise<void>;
+};
+
+export const FeedContext = createContext<FeedContextProps>({
+  status: STATUS.IDLE,
+  error: '',
+  feed: [],
+  updateFeed: async () => {},
+});
+
+type FeedProviderProps = {
+  children: ReactNode;
+};
+
+const FeedProvider: FC<FeedProviderProps> = ({ children }) => {
+  const [status, setStatus] = useState(STATUS.IDLE);
+  const [error, setError] = useState('');
+  const [feed, setFeed] = useState<TPost[]>([]);
+
+  const updateFeed = useCallback(async () => {
+    setStatus(STATUS.LOADING);
+    setFeed([]);
+    try {
+      const { success, error, data } = await api.get<ApiResponse>('/feed');
+      if (success) {
+        setStatus(STATUS.SUCCESS);
+        setFeed(data);
+        return;
+      }
+      setError(error);
+      setStatus(STATUS.ERROR);
+    } catch (error) {
+      setStatus(STATUS.ERROR);
+      setError('Unable to load feed at this time');
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(
+    function fetchFeedOnInitialRender() {
+      if (status === STATUS.IDLE) {
+        updateFeed();
+      }
+    },
+    [status, updateFeed],
+  );
+
+  return (
+    <FeedContext.Provider value={{ status, error, feed, updateFeed }}>
+      {children}
+    </FeedContext.Provider>
+  );
+};
+
+export default FeedProvider;
