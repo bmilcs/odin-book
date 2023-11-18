632beac6a4d88a92161c9f8b2bcc575cd73403de
Bryan Miller <bmilcs@yahoo.com>
2023-11-18 17:04:47 -0500

    Fix naming convention of comment content on server

--- server/src/models/commentModel.ts
+++ server/src/models/commentModel.ts
@@ -3,14 +3,14 @@ import { IUser } from '@/models/userModel';
 import { Document, Schema, model, models } from 'mongoose';
 
 export interface IComment extends Document {
-  text: string;
+  content: string;
   author: IUser['_id'];
   post: IPost['_id'];
 }
 
 const CommentSchema: Schema = new Schema(
   {
-    text: { type: String, required: true, maxlength: 1000 },
+    content: { type: String, required: true, maxlength: 1000 },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
   },
