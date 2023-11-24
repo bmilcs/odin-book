f747153651ab95e3a134f3b786df2419abec9e7c
Bryan Miller <bmilcs@yahoo.com>
2023-11-24 17:29:10 -0500

    Refactor LikeButton component w/ like counts

--- client/src/components/common/like-button.tsx
+++ client/src/components/common/like-button.tsx
@@ -1,27 +1,44 @@
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
-import { FC, useState } from 'react';
+import useLike from '@/hooks/useLike';
+import { FC } from 'react';
 
 type LikeButtonProps = {
   isLiked: boolean;
-  id: string;
+  _id: string;
   contentType: 'post' | 'comment';
+  likeCount: number;
 };
 
+const LikeButton: FC<LikeButtonProps> = ({
+  isLiked,
+  _id,
+  contentType,
+  likeCount,
+}) => {
+  const { status, error, likeStatus, toggleLike, totalLikes } = useLike({
+    _id,
+    contentType,
+    isLiked,
+    likeCount,
+  });
 
-const LikeButton: FC<LikeButtonProps> = ({ isLiked, id, contentType }) => {
-  const [likeStatus, setLikeStatus] = useState(isLiked);
-
-  
-
-  return isLiked ? (
-    <Button variant={'outline'} size={'icon'} onClick={}>
-      <Icons.like />
-    </Button>
-  ) : (
-    <Button variant={'outline'} size={'icon'} onClick={}>
-      <Icons.dislike />
-    </Button>
+  return (
+    <div className="flex items-center gap-2 text-sm">
+      <Button
+        variant={'outline'}
+        size={'icon'}
+        onClick={toggleLike}
+        disabled={status === 'loading' || !!error}
+      >
+        {likeStatus ? (
+          <Icons.like className="text-blue-500 dark:text-blue-400" />
+        ) : (
+          <Icons.dislike />
+        )}
+      </Button>
+      <span>{totalLikes} Likes</span>
+    </div>
   );
 };
 
