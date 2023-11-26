4a1e1fd0e0700aa9e985127b8e156a1633f97883
Bryan Miller <bmilcs@yahoo.com>
2023-11-26 16:04:53 -0500

    Refactor Post component: renders post/comments/buttons

--- client/src/components/common/post.tsx
+++ client/src/components/common/post.tsx
@@ -1,14 +1,18 @@
+import CommentForm from '@/components/common/comment-form';
 import LikeButton from '@/components/common/like-button';
+import PostComment from '@/components/common/post-comment';
 import { AuthContext } from '@/components/services/auth-provider';
 import { TPost } from '@/components/services/feed-provider';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Icons } from '@/components/ui/icons';
+import useExistingPost from '@/hooks/useExistingPost';
 import { formatDate } from '@/utils/formatters';
 import { useContext } from 'react';
 
@@ -20,25 +24,66 @@ const Post = ({
   data: TPost;
   className?: string;
 }) => {
+  const { deletePost, updatePost } = useExistingPost({ postId: data._id });
   const { user } = useContext(AuthContext);
-  const isLikedByUser = data.likes.includes(user._id) || false;
-  const likeCount = data.likes.length;
+  const initialPostLikeCount = data.likes.length;
+  const initialIsLikedByUser = data.likes.some(
+    (like) => like.user._id.toString() === user?._id.toString(),
+  );
+  const isPostCreatedByUser =
+    data.author._id.toString() === user?._id.toString();
+
+  console.log(data.comments);
 
   return (
     <Card className={className} {...props}>
-      <CardHeader>
+      {/* Post Author & Date Posted */}
+      <CardHeader className="flex items-start">
         <div className="space-y-1">
-          <CardTitle>{data.author.username} </CardTitle>
+          <CardTitle>{data.author.username}</CardTitle>
           <CardDescription className="ml-auto font-normal">
             {formatDate(data.createdAt)}
           </CardDescription>
         </div>
       </CardHeader>
-      <CardContent>{data.content}</CardContent>
-      <CardFooter>
-        <LikeButton isLiked={isLikedByUser} />
-        <span className="ml-2 text-sm">{likeCount} Likes</span>
-      </CardFooter>
+      {/* Post content */}
+      <CardContent>
+        <p>{data.content}</p>
+      </CardContent>
+      {/* Buttons */}
+      <CardContent className="border-y-2 py-2">
+        <div className="flex justify-between">
+          {/* Like Button */}
+          <LikeButton
+            isLiked={initialIsLikedByUser}
+            postId={data._id}
+            contentType="post"
+            likeCount={initialPostLikeCount}
+          />
+          {/* OP Action Buttons: Edit/Delete */}
+          {isPostCreatedByUser && (
+            <div className="flex">
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => updatePost({ content: 'New Post Data!' })}
+              >
+                <Icons.edit />
+              </Button>
+              <Button variant="ghost" onClick={() => deletePost()} size="icon">
+                <Icons.delete />
+              </Button>
+            </div>
+          )}
+        </div>
+      </CardContent>
+      {/* Comments */}
+      <CardContent className="grid gap-4 py-4">
+        <CommentForm postId={data._id} className="" />
+        {data.comments.map((comment) => (
+          <PostComment key={comment._id} data={comment} className="pl-4" />
+        ))}
+      </CardContent>
     </Card>
   );
 };
