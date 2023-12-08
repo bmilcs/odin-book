d34588fc76171f4a0a05405c55434a821794518f
Bryan Miller <bmilcs@yahoo.com>
2023-12-08 16:09:43 -0500

    Remove unused env vars from client

--- client/src/utils/env.ts
+++ client/src/utils/env.ts
@@ -1,12 +1,5 @@
 // this file is used to export all the environment variables
 
 export const API_BASE_URL = import.meta.env.VITE_API_ENDPOINT ?? '';
-console.log('DEBUG: API_BASE_URL: ', API_BASE_URL);
-
-export const PRODUCTION_DOMAIN =
-  (import.meta.env.VITE_PRODUCTION_DOMAIN as string) ?? '';
-
-export const DEVELOPMENT_DOMAIN =
-  (import.meta.env.VITE_DEVELOPMENT_DOMAIN as string) ?? 'localhost';
 
 export const CLIENT_MODE = import.meta.env.MODE;
