bf71b1c3acc094cd4282478b0ce51acf1f6e470f
Bryan Miller <bmilcs@yahoo.com>
2023-12-08 15:54:21 -0500

    Add console.log to env on client for debugging GH Action

--- client/src/utils/env.ts
+++ client/src/utils/env.ts
@@ -1,6 +1,7 @@
 // this file is used to export all the environment variables
 
 export const API_BASE_URL = import.meta.env.VITE_API_ENDPOINT ?? '';
+console.log('DEBUG: API_BASE_URL: ', API_BASE_URL);
 
 export const PRODUCTION_DOMAIN =
   (import.meta.env.VITE_PRODUCTION_DOMAIN as string) ?? '';
