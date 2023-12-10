d2f011ca2b5f15452f71b9c55da25b0610a0b37f
Bryan Miller <bmilcs@yahoo.com>
2023-12-10 11:51:22 -0500

    Add redirect for authenticated user on home page
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/components/pages/home-page.tsx
+++ client/src/components/pages/home-page.tsx
@@ -1,4 +1,13 @@
+import { AuthContext } from '@/components/services/auth-provider';
+import { useContext, useEffect } from 'react';
+
 const HomePage = () => {
+  const { redirectAuthenticatedUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    redirectAuthenticatedUser('/feed');
+  }, [redirectAuthenticatedUser]);
+
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <h1 className="text-4xl font-black">FriendLink</h1>
