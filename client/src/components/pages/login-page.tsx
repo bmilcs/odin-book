1915135401237464e6fb01622c2e84f96b5e6813
Bryan Miller <bmilcs@yahoo.com>
2023-12-10 11:46:02 -0500

    Add redirect for authenticated users in login and signup pages
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/components/pages/login-page.tsx
+++ client/src/components/pages/login-page.tsx
@@ -1,6 +1,14 @@
 import LoginForm from '@/components/common/login-form';
+import { AuthContext } from '@/components/services/auth-provider';
+import { useContext, useEffect } from 'react';
 
 const LoginPage = () => {
+  const { redirectAuthenticatedUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    redirectAuthenticatedUser('/feed');
+  }, [redirectAuthenticatedUser]);
+
   return (
     <div className="grid h-full place-items-center">
       <LoginForm className="m-auto min-w-[min(400px,80vw)] max-w-3xl" />
--- client/src/components/pages/signup-page.tsx
+++ client/src/components/pages/signup-page.tsx
@@ -1,6 +1,14 @@
 import SignupForm from '@/components/common/signup-form';
+import { AuthContext } from '@/components/services/auth-provider';
+import { useContext, useEffect } from 'react';
 
 const SignupPage = () => {
+  const { redirectAuthenticatedUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    redirectAuthenticatedUser('/feed');
+  }, [redirectAuthenticatedUser]);
+
   return (
     <div className="grid h-full place-items-center">
       <SignupForm className="m-auto min-w-[min(400px,80vw)] max-w-3xl" />;
