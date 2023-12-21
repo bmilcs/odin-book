224287fa26e187589bc74095fca986399650465a
Bryan Miller <bmilcs@yahoo.com>
2023-12-21 17:09:06 -0500

    Add notificationModel to server

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,3 +5,5 @@ export { default as commentModel } from './commentModel';
 export { default as likeModel } from './likeModel';
 
 export { default as postModel } from './postModel';
+
+export { default as notificationModel } from './notificationModel';
---
+++ server/src/models/notificationModel.ts
@@ -0,0 +1,58 @@
+import { IComment } from '@/models/commentModel';
+import { IUser } from '@/models/userModel';
+import mongoose, { Document, Schema, model, models } from 'mongoose';
+
+export enum NotificationType {
+  INCOMING_FRIEND_REQUEST = 'incoming_friend_request',
+  ACCEPTED_FRIEND_REQUEST = 'accepted_friend_request',
+  NEW_COMMENT = 'new_comment',
+}
+
+export interface INotification extends Document {
+  type: NotificationType;
+  fromUser: IUser['_id'];
+  toUser: IUser['_id'];
+  post?: IComment['_id'];
+}
+
+const notificationSchema: Schema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: Object.values(NotificationType),
+      required: true,
+    },
+    fromUser: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    toUser: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    post: { type: mongoose.Types.ObjectId, ref: 'Comment' },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }, // auto create 'createdAt' and 'updatedAt' fields
+);
+
+// add notification to user's notifications array automatically
+notificationSchema.pre<INotification>('save', async function (next) {
+  const UserModel = models['User'];
+  if (UserModel) {
+    await UserModel.updateOne(
+      { _id: this.toUser },
+      { $addToSet: { notifications: this._id } },
+    );
+  }
+  next();
+});
+
+// delete notification from user's notifications array automatically
+notificationSchema.pre<INotification>('deleteOne', async function (next) {
+  const UserModel = models['User'];
+  if (UserModel) {
+    await UserModel.updateOne(
+      { _id: this.toUser },
+      { $pull: { notifications: this._id } },
+    );
+  }
+  next();
+});
+
+export default models['Notification'] ||
+  model<INotification>('Notification', notificationSchema);
