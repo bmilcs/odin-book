808c8a8eff1f81b68b5b0c296a6d1a04b79902a6
Bryan Miller <bmilcs@yahoo.com>
2023-12-22 16:11:59 -0500

    Refactor notificationRouter tests on server
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- server/src/tests/notificationRouter.spec.ts
+++ server/src/tests/notificationRouter.spec.ts
@@ -1,10 +1,12 @@
-import { notificationModel } from '@/models';
+import { notificationModel, userModel } from '@/models';
 import {
   NONEXISTENT_MONGODB_ID,
   USER_ONE,
   USER_THREE,
   USER_TWO,
   app,
+  deleteFriendsAndRequestsFromAllTestUsers,
+  deleteNotificationsFromAllTestUsers,
 } from '@/tests/setup';
 import { expect } from 'chai';
 import { describe } from 'mocha';
@@ -17,9 +19,7 @@ describe('NOTIFICATION ROUTER', () => {
 
   describe('GET /notifications', () => {
     before(async () => {
-      // delete all notifications
-      await notificationModel.deleteMany({});
-
+      await deleteNotificationsFromAllTestUsers();
       // send friend request from user two to user one
       await request(app)
         .post(`/friends/send-request/${USER_ONE._id}`)
@@ -27,8 +27,8 @@ describe('NOTIFICATION ROUTER', () => {
     });
 
     after(async () => {
-      // delete all notifications
-      await notificationModel.deleteMany({});
+      await deleteNotificationsFromAllTestUsers();
+      await deleteFriendsAndRequestsFromAllTestUsers();
     });
 
     it('should return 401 if user is not logged in', async () => {
@@ -57,6 +57,7 @@ describe('NOTIFICATION ROUTER', () => {
       expect(body.data[0].toUser).to.equal(USER_ONE._id);
       expect(body.data[0].fromUser).to.contain({ _id: USER_TWO._id });
       expect(body.data[0].type).to.equal('incoming_friend_request');
+
       // check db to make sure notification was created
       const notification = await notificationModel.findOne({
         toUser: USER_ONE._id,
@@ -78,16 +79,28 @@ describe('NOTIFICATION ROUTER', () => {
 
   describe('GET /notifications/unread', () => {
     before(async () => {
-      // delete all notifications
-      await notificationModel.deleteMany({});
-      // send friend request from user two to user one & user three to user one
+      await deleteNotificationsFromAllTestUsers();
+      // send friend requests to user one from user two/three
       await request(app)
         .post(`/friends/send-request/${USER_ONE._id}`)
         .set('Cookie', USER_TWO.jwtCookie);
       await request(app)
         .post(`/friends/send-request/${USER_ONE._id}`)
         .set('Cookie', USER_THREE.jwtCookie);
-      // ! MARK ONE NOTIFICATION AS READ
+      // get id of a single notification
+      const { body: nBody } = await request(app)
+        .get('/notifications')
+        .set('Cookie', USER_ONE.jwtCookie);
+      const notificationId = nBody.data[0]._id.toString();
+      // mark one of the notifications as read
+      await request(app)
+        .put(`/notifications/${notificationId}/read`)
+        .set('Cookie', USER_ONE.jwtCookie);
+    });
+
+    after(async () => {
+      await deleteNotificationsFromAllTestUsers();
+      await deleteFriendsAndRequestsFromAllTestUsers();
     });
 
     it('should return 401 if user is not logged in', async () => {
@@ -95,6 +108,30 @@ describe('NOTIFICATION ROUTER', () => {
       expect(statusCode).to.equal(401);
       expect(body.success).to.be.false;
       expect(body.error).to.equal('Unauthorized');
+    });
+
+    it('should return 201 if unread notifications are fetched successfully w/ db check', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/notifications/unread')
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(201);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal(
+        'Unread notifications fetched successfully',
+      );
+      expect(body.data).to.be.an('array');
+      expect(body.data.length).to.equal(1);
+      expect(body.data[0]).to.have.property('_id');
+      expect(body.data[0]).to.have.property('type');
+      expect(body.data[0]).to.have.property('fromUser');
+      expect(body.data[0]).to.have.property('toUser');
+      expect(body.data[0]).to.not.have.property('post');
+      expect(body.data[0]).to.have.property('createdAt');
+      expect(body.data[0]).to.have.property('updatedAt');
+      expect(body.data[0].read).to.be.false;
+      expect(body.data[0].toUser).to.equal(USER_ONE._id);
+      expect(body.data[0].fromUser).to.contain({ _id: USER_THREE._id });
+      expect(body.data[0].type).to.equal('incoming_friend_request');
     });
   });
 
@@ -104,18 +141,16 @@ describe('NOTIFICATION ROUTER', () => {
 
   describe('PUT /notifications/:notificationId/read', () => {
     before(async () => {
-      // delete all notifications
-      await notificationModel.deleteMany({});
-      // send friend request from user two to user one
-      const res = await request(app)
+      await deleteFriendsAndRequestsFromAllTestUsers();
+      await deleteNotificationsFromAllTestUsers();
+      // send friend request from user one to user two
+      await request(app)
         .post(`/friends/send-request/${USER_ONE._id}`)
         .set('Cookie', USER_TWO.jwtCookie);
-      console.log(res.body);
     });
 
     after(async () => {
-      // delete all notifications
-      await notificationModel.deleteMany({});
+      await deleteNotificationsFromAllTestUsers();
     });
 
     it('should return 401 if user is not logged in', async () => {
@@ -155,24 +190,29 @@ describe('NOTIFICATION ROUTER', () => {
     });
 
     it('should return 200 if notification is marked as read successfully', async () => {
-      const { body: nBody, status } = await request(app)
+      const { body: nBody } = await request(app)
         .get('/notifications')
         .set('Cookie', USER_ONE.jwtCookie);
-      // TODO
-      // ! Ensure read notifications are not returned in GET /notifications
-      // ! Ensure user's notification [] is updated
-      console.log(nBody);
-      const notificationId = nBody.data[0];
-      // const { statusCode, body } = await request(app)
-      //   .put(`/notifications/${notificationId}/read`)
-      //   .set('Cookie', USER_ONE.jwtCookie);
-      // expect(statusCode).to.equal(200);
-      // expect(body.success).to.be.true;
-      // expect(body.message).to.equal('Marked notification as read successfully');
-      // expect(body.data).to.be.null;
-      // // check db to make sure notification was marked as read
-      // const notification = await notificationModel.findById(notificationId);
-      // expect(notification.read).to.be.true;
+      const notificationId = nBody.data[0]._id.toString();
+
+      const { statusCode, body } = await request(app)
+        .put(`/notifications/${notificationId}/read`)
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(200);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal('Marked notification as read successfully');
+      expect(body.data).to.be.null;
+
+      // check db to make sure notification was marked as read
+      const notification = await notificationModel.findById(notificationId);
+      expect(notification.read).to.be.true;
+
+      // check db to make sure user's notifications [] was updated
+      const user = await userModel
+        .findById(USER_ONE._id)
+        .populate('notifications');
+      expect(user.notifications.length).to.equal(1);
+      expect(user.notifications[0].read).to.be.true;
     });
   });
 
@@ -181,6 +221,22 @@ describe('NOTIFICATION ROUTER', () => {
   //
 
   describe('PUT /notifications/read-all', () => {
+    before(async () => {
+      await deleteFriendsAndRequestsFromAllTestUsers();
+      await deleteNotificationsFromAllTestUsers();
+      // send friend request to user one from two/three
+      await request(app)
+        .post(`/friends/send-request/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+      await request(app)
+        .post(`/friends/send-request/${USER_ONE._id}`)
+        .set('Cookie', USER_THREE.jwtCookie);
+    });
+
+    after(async () => {
+      await deleteNotificationsFromAllTestUsers();
+    });
+
     it('should return 401 if user is not logged in', async () => {
       const { statusCode, body } = await request(app).put(
         '/notifications/read-all',
@@ -189,6 +245,34 @@ describe('NOTIFICATION ROUTER', () => {
       expect(body.success).to.be.false;
       expect(body.error).to.equal('Unauthorized');
     });
+
+    it('should return 200 if all notifications are marked as read successfully', async () => {
+      const { statusCode, body } = await request(app)
+        .put('/notifications/read-all')
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(200);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal(
+        'Marked all notifications as read successfully',
+      );
+      expect(body.data).to.be.null;
+
+      // check db to make sure notifications were marked as read
+      const notifications = await notificationModel.find({
+        toUser: USER_ONE._id,
+      });
+      expect(notifications.length).to.equal(2);
+      expect(notifications[0].read).to.be.true;
+      expect(notifications[1].read).to.be.true;
+
+      // check db to make sure user's notifications [] was updated
+      const user = await userModel
+        .findById(USER_ONE._id)
+        .populate('notifications');
+      expect(user.notifications.length).to.equal(2);
+      expect(user.notifications[0].read).to.be.true;
+      expect(user.notifications[1].read).to.be.true;
+    });
   });
 
   //
@@ -196,6 +280,26 @@ describe('NOTIFICATION ROUTER', () => {
   //
 
   describe('DELETE /notifications/:notificationId', () => {
+    let notificationId: string;
+
+    before(async () => {
+      await deleteFriendsAndRequestsFromAllTestUsers();
+      await deleteNotificationsFromAllTestUsers();
+      // send friend request from user one to user two
+      await request(app)
+        .post(`/friends/send-request/${USER_ONE._id}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+      // get id of a single notification
+      const { body: nBody } = await request(app)
+        .get('/notifications')
+        .set('Cookie', USER_ONE.jwtCookie);
+      notificationId = nBody.data[0]._id.toString();
+    });
+
+    after(async () => {
+      await deleteNotificationsFromAllTestUsers();
+    });
+
     it('should return 401 if user is not logged in', async () => {
       const { statusCode, body } = await request(app).delete(
         '/notifications/:notificationId',
@@ -203,6 +307,53 @@ describe('NOTIFICATION ROUTER', () => {
       expect(statusCode).to.equal(401);
       expect(body.success).to.be.false;
       expect(body.error).to.equal('Unauthorized');
+    });
+
+    it('should return 400 if notification ID is invalid', async () => {
+      const { statusCode, body } = await request(app)
+        .delete('/notifications/12345')
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Notification ID is invalid');
+    });
+
+    it('should return 400 if notification ID is nonexistent', async () => {
+      const { statusCode, body } = await request(app)
+        .delete(`/notifications/${NONEXISTENT_MONGODB_ID}`)
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Notification not found');
+    });
+
+    it('should return 400 if notification is not for logged in user', async () => {
+      const { statusCode, body } = await request(app)
+        .delete(`/notifications/${notificationId}`)
+        .set('Cookie', USER_TWO.jwtCookie);
+      expect(statusCode).to.equal(400);
+      expect(body.success).to.be.false;
+      expect(body.error).to.equal('Notification not found');
+    });
+
+    it('should return 200 if notification is deleted successfully', async () => {
+      const { statusCode, body } = await request(app)
+        .delete(`/notifications/${notificationId}`)
+        .set('Cookie', USER_ONE.jwtCookie);
+      expect(statusCode).to.equal(200);
+      expect(body.success).to.be.true;
+      expect(body.message).to.equal('Deleted notification successfully');
+      expect(body.data).to.be.null;
+
+      // check db to make sure notification was deleted
+      const notification = await notificationModel.findById(notificationId);
+      expect(notification).to.be.null;
+
+      // check db to make sure user's notifications [] was updated
+      const user = await userModel
+        .findById(USER_ONE._id)
+        .populate('notifications');
+      expect(user.notifications.length).to.equal(0);
     });
   });
 });
