35c46dfc23ccf974a48959070095a45eebb9171e
Bryan Miller <bmilcs@yahoo.com>
2024-01-02 16:22:57 -0500

    Add eslint/react package & eslintrc settings to client

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+root: true,
+env: { browser: true, es2020: true },
+extends: [
+'eslint:recommended',
+'plugin:@typescript-eslint/recommended',
+'plugin:react-hooks/recommended',
+"plugin:react/recommended",
+],
+settings: {
+"react": {
+"version": "detect",}
+},
+ignorePatterns: ['dist', '.eslintrc.cjs', '/src/components/ui/**/*', '/src/components/services/theme-provider.tsx'],
+parser: '@typescript-eslint/parser',
+plugins: ['react-refresh'],
+rules: {
+"react/no-array-index-key": "error",
+"react/react-in-jsx-scope": "off",
+'react-refresh/only-export-components': [
+'warn',
+{
+  allowConstantExport: true },
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
   },
 }
