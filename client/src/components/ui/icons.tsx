f373e57de6a4742b9eeeeefaffce583013d62491
Bryan Miller <bmilcs@yahoo.com>
2024-01-03 16:43:36 -0500

    Add more to Icons component

--- client/src/components/ui/icons.tsx
+++ client/src/components/ui/icons.tsx
@@ -309,4 +309,18 @@ export const Icons = {
       />
     </svg>
   ),
+  more: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M12 3c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m0 14c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m0-7c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2"
+      />
+    </svg>
+  ),
 };
