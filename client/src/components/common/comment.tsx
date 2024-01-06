57275b105a539a47f87c7464a65694f0f2e16f99
Bryan Miller <bmilcs@yahoo.com>
2024-01-06 16:59:16 -0500

    Refactor comment component layout and styling
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/components/common/comment.tsx
+++ client/src/components/common/comment.tsx
@@ -41,53 +41,58 @@ const Comment = ({
   };
 
   return (
-    <div key={data._id} className={`${className} flex justify-between`}>
-      {/* Comment Author & Date Posted */}
-      <div className="w-full">
-        <div className="text-xs text-gray-400">
-          <Link to={`/users/${data.author.username}`}>
-            {data.author.username}
-          </Link>{' '}
-          {data.updatedAt !== data.createdAt
-            ? `edited ${formatDate(data.updatedAt)}`
-            : `posted ${formatDate(data.createdAt)}`}
-        </div>
+    <div key={data._id}>
+      <div
+        className={`${className} ${
+          editCommentMode ? 'w-full' : 'w-max'
+        } rounded-xl bg-accent p-3`}
+      >
+        {/* Comment Author & Date Posted */}
+        <div className="w-full">
+          <div className="text-xs text-foreground">
+            <Link to={`/users/${data.author.username}`} className="font-bold">
+              {data.author.username}
+            </Link>{' '}
+            {data.updatedAt !== data.createdAt
+              ? `edited ${formatDate(data.updatedAt)}`
+              : `posted ${formatDate(data.createdAt)}`}
+          </div>
 
-        {/* Comment Content */}
-        {editCommentMode ? (
-          <CommentEditForm
-            commentId={data._id}
-            postId={data.post}
-            commentContent={data.content}
-            onSuccess={handleSuccessfulEdit}
-            className="mt-2"
-          />
-        ) : (
-          <span className="text-sm">{data.content}</span>
-        )}
-
-        {/* Buttons */}
-        <div className="flex">
-          {/* Like Button */}
-          <LikeButton
-            isLiked={initialIsLikedByUser}
-            likeCount={initialLikeCount}
-            postId={data.post}
-            commentId={data._id}
-            contentType="comment"
-          />
-          {/* OP Actions: Edit/Delete */}
-          {isCommentCreatedByUser && (
-            <div className="ml-8 flex gap-2">
-              <Button variant="link" onClick={handleEditComment} size="icon">
-                Edit
-              </Button>
-              <Button variant="link" onClick={handleDeleteComment} size="icon">
-                Delete
-              </Button>
-            </div>
+          {/* Comment Content */}
+          {editCommentMode ? (
+            <CommentEditForm
+              commentId={data._id}
+              postId={data.post}
+              commentContent={data.content}
+              onSuccess={handleSuccessfulEdit}
+              className="mt-2 w-full"
+            />
+          ) : (
+            <span>{data.content}</span>
           )}
         </div>
+      </div>
+      {/* Buttons */}
+      <div className="flex">
+        {/* Like Button */}
+        <LikeButton
+          isLiked={initialIsLikedByUser}
+          likeCount={initialLikeCount}
+          postId={data.post}
+          commentId={data._id}
+          contentType="comment"
+        />
+        {/* OP Actions: Edit/Delete */}
+        {isCommentCreatedByUser && (
+          <div className="ml-2 flex gap-2">
+            <Button variant="link" onClick={handleEditComment} size="icon">
+              Edit
+            </Button>
+            <Button variant="link" onClick={handleDeleteComment} size="icon">
+              Delete
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
