a8064aab97bcb288eaf436fb45ae1a4de1600739
Bryan Miller <bmilcs@yahoo.com>
2024-01-14 12:42:55 -0500

    Refactor user profile retrieval and update logic
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -21,17 +21,26 @@ const getProfile = tryCatch(
 
     if (isProfileOwner || isAFriend) {
       // get full user profile
-      const user = await userModel.findOne({ username }, { password: 0 });
+      const user = await userModel
+        .findOne(
+          { username },
+          {
+            password: 0,
+            notifications: 0,
+            friendRequestsSent: 0,
+            friendRequestsReceived: 0,
+          },
+        )
+        .populate('friends');
       if (!user) {
         return next(new AppError('User not found', 400));
       }
       res.success('Full user profile fetched successfully', user, 201);
     } else {
       // get partial user profile
-      const user = await userModel.findOne(
-        { username },
-        { _id: 1, username: 1 },
-      );
+      const user = await userModel
+        .findOne({ username }, { _id: 1, username: 1, friends: 1 })
+        .populate('friends');
       if (!user) {
         return next(new AppError('User not found', 400));
       }
@@ -42,7 +51,44 @@ const getProfile = tryCatch(
 
 const updateProfile = tryCatch(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.success('User profile updated successfully', null, 201);
+    const { userId } = req;
+    const { username } = req.params;
+    // get info for requesting user
+    const requestingUser = await userModel.findById(userId, {
+      password: 0,
+    });
+    if (!requestingUser) {
+      return next(new AppError('Your user information was not found', 400));
+    }
+    // make sure requesting user is the profile owner
+    if (requestingUser.username !== username) {
+      return next(new AppError('You are not the profile owner', 403));
+    }
+    // make sure new username is not taken
+    const { username: newUsername } = req.body;
+    if (newUsername !== username) {
+      const isDuplicate = await userModel.exists({ username: newUsername });
+      if (isDuplicate) {
+        return next(new AppError('Username already in use', 409));
+      }
+    }
+    // make sure new email is not taken
+    const { email: newEmail } = req.body;
+    if (newEmail !== requestingUser.email) {
+      const isDuplicate = await userModel.exists({ email: newEmail });
+      if (isDuplicate) {
+        return next(new AppError('Email already in use', 409));
+      }
+    }
+    // update user profile
+    const { bio, location } = req.body;
+    requestingUser.profile.bio = bio;
+    requestingUser.profile.location = location;
+    requestingUser.email = newEmail;
+    requestingUser.username = newUsername;
+    requestingUser.markModified('profile');
+    await requestingUser.save();
+    res.success('User profile updated successfully', requestingUser, 201);
   },
 );
 
