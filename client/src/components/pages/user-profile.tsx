4d0fe412b7c6728ac7705de438eefbe9aa6f9a17
Bryan Miller <bmilcs@yahoo.com>
2024-01-18 15:55:04 -0500

    Add redirect for unauthenticated users in UserProfilePage
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/components/pages/user-profile.tsx
+++ client/src/components/pages/user-profile.tsx
@@ -1,6 +1,14 @@
 import UserProfile from '@/components/common/user-profile';
+import { AuthContext } from '@/components/services/auth-provider';
+import { useContext, useEffect } from 'react';
 
 const UserProfilePage = () => {
+  const { redirectUnauthenticatedUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    redirectUnauthenticatedUser('/login');
+  }, [redirectUnauthenticatedUser]);
+
   return (
     <>
       <UserProfile />
