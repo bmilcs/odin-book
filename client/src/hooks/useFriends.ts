4f71456494d7a39eead655467fc15f0d6fc99adc
Bryan Miller <bmilcs@yahoo.com>
2024-02-09 17:57:43 -0500

    Add cancelFriendRequest & user utils to useFriends hook
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/hooks/useFriends.ts
+++ client/src/hooks/useFriends.ts
@@ -3,7 +3,7 @@ import { FeedContext } from '@/components/services/feed-provider';
 import api from '@/utils/api';
 import STATUS from '@/utils/constants';
 import { getErrorMsg } from '@/utils/errors';
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 
 type ApiResponse = {
   success: boolean;
@@ -15,7 +15,7 @@ const useFriends = () => {
   const [status, setStatus] = useState(STATUS.IDLE);
   const [error, setError] = useState('');
   const { updateFeed } = useContext(FeedContext);
-  const { updateUserData } = useContext(AuthContext);
+  const { user, updateUserData } = useContext(AuthContext);
 
   const sendFriendRequest = async (userId: string) => {
     setStatus(STATUS.LOADING);
@@ -31,6 +31,35 @@ const useFriends = () => {
       const { success, error } = await api.post<ApiResponse>(
         `/friends/send-request/${userId}`,
         {},
+      );
+      if (success) {
+        setStatus(STATUS.SUCCESS);
+        await updateUserData();
+        return;
+      }
+      setStatus(STATUS.ERROR);
+      setError(error);
+    } catch (error) {
+      setStatus(STATUS.ERROR);
+      const errorMsg = getErrorMsg(error);
+      setError(errorMsg);
+      console.log(error);
+    }
+  };
+
+  const cancelFriendRequest = async (userId: string) => {
+    setStatus(STATUS.LOADING);
+    setError('');
+
+    if (!userId) {
+      setStatus(STATUS.ERROR);
+      setError('User ID is required');
+      return;
+    }
+
+    try {
+      const { success, error } = await api.del<ApiResponse>(
+        `/friends/cancel-request/${userId}`,
       );
       if (success) {
         setStatus(STATUS.SUCCESS);
@@ -138,13 +167,50 @@ const useFriends = () => {
     }
   };
 
+  const isUserAFriend = useCallback(
+    (username: string) => {
+      return user!.friends.some((friend) => friend.username === username);
+    },
+    [user],
+  );
+
+  const isUserInIncomingFriendRequests = useCallback(
+    (username: string) => {
+      return user!.friendRequestsReceived.some(
+        (request) => request.username === username,
+      );
+    },
+    [user],
+  );
+
+  const isUserInOutgoingFriendRequests = useCallback(
+    (username: string) => {
+      return user!.friendRequestsSent.some(
+        (request) => request.username === username,
+      );
+    },
+    [user],
+  );
+
+  const isUserTheActiveUser = useCallback(
+    (username: string) => {
+      return user!.username === username;
+    },
+    [user],
+  );
+
   return {
     status,
     error,
     sendFriendRequest,
+    cancelFriendRequest,
     acceptFriendRequest,
     rejectFriendRequest,
     deleteFriend,
+    isUserTheActiveUser,
+    isUserAFriend,
+    isUserInIncomingFriendRequests,
+    isUserInOutgoingFriendRequests,
   };
 };
 
