ceba234cdd5808faf7653e1aa2b5ed4a2f906dfe
Bryan Miller <bmilcs@yahoo.com>
2024-02-14 17:37:46 -0500

    Add UserProfileImage component
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/components/common/user-profile-image.tsx
+++ client/src/components/common/user-profile-image.tsx
@@ -1,30 +1,44 @@
 import { Icons } from '@/components/ui/icons';
-import { AuthContext } from '@/context/auth-provider';
+import { AuthContext, TUser } from '@/context/auth-provider';
+import { API_BASE_URL } from '@/utils/env';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 type UserProfileProps = {
+  user?: TUser | null;
   className?: string;
 };
 
-const UserProfileImage = ({ className = '', ...props }: UserProfileProps) => {
-  const { user } = useContext(AuthContext);
+const UserProfileImage = ({
+  user: propUser,
+  className = '',
+  ...props
+}: UserProfileProps) => {
+  // if propUser is not provided, use the active user from auth context
+  const { user: activeUser } = useContext(AuthContext);
+  const user = propUser || activeUser;
 
   if (user?.photo) {
+    const imgUrl = `${API_BASE_URL}${user.photo}`;
     return (
-      <img
-        src={user.photo}
-        alt="Profile Image"
-        className={`${className}`}
-        {...props}
-      />
+      <Link to={`/users/${user.username}`}>
+        <img
+          src={imgUrl}
+          alt={`${user.username}'s profile image`}
+          className={`aspect-square object-cover shadow ${className}`}
+          {...props}
+        />
+      </Link>
     );
   }
 
   return (
-    <Icons.userPlaceholder
-      className={`rounded-xl bg-slate-100 fill-slate-300 text-primary dark:bg-accent ${className}`}
-      {...props}
-    />
+    <Link to={`/users/${user?.username}`}>
+      <Icons.userPlaceholder
+        className={`rounded-xl bg-slate-100 fill-slate-300 text-primary shadow dark:bg-accent ${className}`}
+        {...props}
+      />
+    </Link>
   );
 };
 
