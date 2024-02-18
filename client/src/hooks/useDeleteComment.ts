de128b72942b7a06abf6ba0fe6b53578d1e074d3
Bryan Miller <bmilcs@yahoo.com>
2024-02-18 15:29:27 -0500

    Add RemoveCommentToFeed to FeedContext
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/hooks/useDeleteComment.ts
+++ client/src/hooks/useDeleteComment.ts
@@ -1,3 +1,4 @@
+import { useFeedContext } from '@/hooks/useFeedContext';
 import api from '@/utils/api';
 import STATUS from '@/utils/constants';
 import { getErrorMsg } from '@/utils/errors';
@@ -5,6 +6,8 @@ import { TApiResponse, TComment } from '@/utils/types';
 import { useState } from 'react';
 
 const useDeleteComment = () => {
+  const { removeCommentFromFeed } = useFeedContext();
+
   const [status, setStatus] = useState(STATUS.IDLE);
   const [error, setError] = useState('');
   const [commentData, setCommentData] = useState<TComment | null>(null);
@@ -31,6 +34,8 @@ const useDeleteComment = () => {
       );
       if (success) {
         setStatus(STATUS.SUCCESS);
+        setCommentData(null);
+        removeCommentFromFeed(postId, commentId);
         return;
       }
       setStatus(STATUS.ERROR);
