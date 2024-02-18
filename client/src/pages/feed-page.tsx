578b96da99879d23b687451cd32be24f90091239
Bryan Miller <bmilcs@yahoo.com>
2024-02-18 15:12:12 -0500

    Add FriendsList & style FeedPage
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/pages/feed-page.tsx
+++ client/src/pages/feed-page.tsx
@@ -1,17 +1,27 @@
 import Feed from '@/components/common/feed';
+import FeedUserSummary from '@/components/common/feed-user-summary';
+import FriendsList from '@/components/common/friends-list';
 import PostNewForm from '@/components/common/post-new-form';
-import UserSummary from '@/components/common/user-summary';
-import CenterColumnContainer from '@/components/layout/center-column-container';
 
 const FeedPage = () => {
   return (
-    <CenterColumnContainer className="">
+    <>
       <div className="grid gap-4 lg:grid-cols-[350px_1fr]">
-        <PostNewForm className="order-0 h-min lg:order-none lg:col-span-2" />
-        <UserSummary className="h-min" />
-        <Feed className="grid gap-4" />
+        <div className="space-y-4">
+          <FeedUserSummary className="h-min" />
+
+          <div className="hidden pl-4 lg:block">
+            <h2 className="mb-2 text-xl font-bold">Friends</h2>
+            <FriendsList />
+          </div>
+        </div>
+
+        <div className="space-y-4">
+          <PostNewForm className="order-0 h-min lg:order-none lg:col-span-2" />
+          <Feed className="grid gap-4" />
+        </div>
       </div>
-    </CenterColumnContainer>
+    </>
   );
 };
 
