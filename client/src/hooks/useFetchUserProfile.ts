a9b9f0ba0c68aff765e0aaf5de1e85b644685af9
Bryan Miller <bmilcs@yahoo.com>
2024-02-19 17:05:45 -0500

    Refactor useFetchProfile: simplify logic & area of concerns
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/hooks/useFetchUserProfile.ts
+++ client/src/hooks/useFetchUserProfile.ts
@@ -1,4 +1,3 @@
-import useUserRelationships from '@/hooks/useUserRelationships';
 import api from '@/utils/api';
 import STATUS from '@/utils/constants';
 import { getErrorMsg } from '@/utils/errors';
@@ -10,79 +9,42 @@ type FetchUserProfileApiResponse = TApiResponse & {
 };
 
 const useFetchUserProfile = () => {
-  const {
-    isUserAFriend,
-    isUserInIncomingFriendRequests,
-    isUserInOutgoingFriendRequests,
-    isUserTheActiveUser,
-  } = useUserRelationships();
-
   const [status, setStatus] = useState(STATUS.IDLE);
   const [error, setError] = useState('');
   const [userProfile, setUserProfile] = useState<TUser | null>(null);
-  const [isFriend, setIsFriend] = useState(false);
-  const [isInOutgoingFriendRequests, setIsInOutgoingFriendRequests] =
-    useState(false);
-  const [isInIncomingFriendRequests, setIsInIncomingFriendRequests] =
-    useState(false);
-  const [isProfileOwner, setIsProfileOwner] = useState(false);
 
-  const fetchUserProfile = useCallback(
-    async (username: string) => {
-      setStatus(STATUS.LOADING);
-      setIsFriend(false);
-      setIsInOutgoingFriendRequests(false);
-      setIsInIncomingFriendRequests(false);
-      setIsProfileOwner(false);
-      setError('');
+  const fetchUserProfile = useCallback(async (username: string) => {
+    setStatus(STATUS.LOADING);
+    setError('');
 
-      if (!username) {
-        setStatus(STATUS.ERROR);
-        setError('Username is required');
+    if (!username) {
+      setStatus(STATUS.ERROR);
+      setError('Username is required');
+      return;
+    }
+
+    try {
+      const { success, data, error } =
+        await api.get<FetchUserProfileApiResponse>(`/users/${username}`);
+      if (success) {
+        setStatus(STATUS.SUCCESS);
+        setUserProfile(data);
         return;
       }
-
-      try {
-        const { success, data, error } =
-          await api.get<FetchUserProfileApiResponse>(`/users/${username}`);
-        if (success) {
-          setStatus(STATUS.SUCCESS);
-          setUserProfile(data);
-          setIsProfileOwner(isUserTheActiveUser(data!.username));
-          setIsFriend(isUserAFriend(data!.username));
-          setIsInOutgoingFriendRequests(
-            isUserInOutgoingFriendRequests(data!.username),
-          );
-          setIsInIncomingFriendRequests(
-            isUserInIncomingFriendRequests(data!.username),
-          );
-          return;
-        }
-        setStatus(STATUS.ERROR);
-        setError(error);
-      } catch (error) {
-        setStatus(STATUS.ERROR);
-        const errorMsg = getErrorMsg(error);
-        setError(errorMsg);
-        console.log(error);
-      }
-    },
-    [
-      isUserAFriend,
-      isUserInIncomingFriendRequests,
-      isUserInOutgoingFriendRequests,
-      isUserTheActiveUser,
-    ],
-  );
+      setStatus(STATUS.ERROR);
+      setError(error);
+    } catch (error) {
+      setStatus(STATUS.ERROR);
+      const errorMsg = getErrorMsg(error);
+      setError(errorMsg);
+      console.log(error);
+    }
+  }, []);
 
   return {
     status,
     error,
     fetchUserProfile,
-    isFriend,
-    isInOutgoingFriendRequests,
-    isInIncomingFriendRequests,
-    isProfileOwner,
     userProfile,
   };
 };
