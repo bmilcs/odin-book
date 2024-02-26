bde982be85339db1dda0ea5f6b90895ad400c585
Bryan Miller <bmilcs@yahoo.com>
2024-02-26 16:25:25 -0500

    Add heading for Edit Profile page
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- client/src/pages/edit-profile-page.tsx
+++ client/src/pages/edit-profile-page.tsx
@@ -4,6 +4,7 @@ import CenterColumnContainer from '@/components/layout/center-column-container';
 const EditProfilePage = () => {
   return (
     <CenterColumnContainer>
+      <h1 className="sr-only">Edit Profile</h1>
       <UserProfileUpdateForm />
     </CenterColumnContainer>
   );
