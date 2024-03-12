3ec9c9020000058a46e560e9a4a660481074cd43
Bryan Miller <bmilcs@yahoo.com>
2024-03-12 16:48:32 -0400

    Refactor fileUploads.ts to handle different types of file uploads
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- server/src/config/fileUploads.ts
+++ server/src/config/fileUploads.ts
@@ -2,20 +2,27 @@ import { STORAGE_VOLUME_PATH } from '@/config/env';
 import { Request } from 'express';
 import multer, { Options } from 'multer';
 
-const store = (destination: string) =>
+const store = (destination: string, type: 'profile' | 'post') =>
   multer.diskStorage({
     destination: (req: Request, file, cb) => {
       cb(null, destination);
     },
     filename: (req: Request, file, cb) => {
       const fileExt = file.originalname.split('.').pop();
+
+      if (type === 'post') {
+        const fileName = `${req.userId!}-${Date.now()}.${fileExt}`;
+        cb(null, fileName);
+        return;
+      }
+
       const fileName = req.userId! + '.' + fileExt;
       cb(null, fileName);
     },
   });
 
 const profileImageUploadOptions: Options = {
-  storage: store(`${STORAGE_VOLUME_PATH}/profile-images`),
+  storage: store(`${STORAGE_VOLUME_PATH}/profile-images`, 'profile'),
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
@@ -26,7 +33,7 @@ const profileImageUploadOptions: Options = {
 };
 
 const postImageUploadOptions: Options = {
-  storage: store(`${STORAGE_VOLUME_PATH}/post-images`),
+  storage: store(`${STORAGE_VOLUME_PATH}/post-images`, 'post'),
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
